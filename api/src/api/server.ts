cd7712037232c8d1f93775b2c3abfeb4045baa61
Aurélien BOCH <aurelien.boch@epitech.eu>
2023-12-08 16:06:35 +0100

    add image upload folder

--- api/src/api/server.ts
+++ api/src/api/server.ts
@@ -4,9 +4,11 @@ import accountRouter from "./account/_router";
 import recipeRouter from "./recipe/_router";
 
 const app = express();
+const images = express.static("images");
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("/images", images);
 app.use("/account", accountRouter);
 app.use("/recipe", recipeRouter);
 
