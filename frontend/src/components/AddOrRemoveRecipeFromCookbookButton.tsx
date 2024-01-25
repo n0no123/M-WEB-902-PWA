85046936f18ab64a024c21a0360fac3bd3d17535
Bruno DE OLIVEIRA <bdoliveira>
2024-01-25 17:31:44 +0000

    Add error handling for AddOrRemoveFromCookbookButton

--- frontend/src/components/AddOrRemoveRecipeFromCookbookButton.tsx
+++ frontend/src/components/AddOrRemoveRecipeFromCookbookButton.tsx
@@ -1,42 +1,70 @@
+import { useCallback, useMemo, useState } from 'react';
+import { useQueryClient } from 'react-query';
 import Button from '@mui/material/Button';
+import Error from './Error';
 import useGetCookBookRecipes from '../api/account/cookbook/useGetCookBookRecipes';
 import useAddRecipeToCookBook from '../api/account/cookbook/useAddRecipeToCookBook';
-import { useCallback, useMemo } from 'react';
 import useRemoveRecipeToCookBook from '../api/account/cookbook/useRemoveRecipeFromCookBook';
-import { useQueryClient } from 'react-query';
 
 type Props = {
     recipeId: string
 }
 
 export default function AddOrRemoveRecipeFromCookbookButton({ recipeId }: Props) {
-    const { data } = useGetCookBookRecipes();
+    const [open, setOpen] = useState<boolean>(false);
+    const [message, setMessage] = useState<string>('');
+
     const queryClient = useQueryClient();
-    const isRecipeInCookbook = useMemo(() => data?.recipesIds.includes(recipeId), [recipeId, data]);
+
     const addRecipe = useAddRecipeToCookBook();
     const removeRecipe = useRemoveRecipeToCookBook();
+    const getCookBookRecipes = useGetCookBookRecipes();
+
+    const isRecipeInCookbook = useMemo(() => {
+        if (getCookBookRecipes.isSuccess) {
+            return getCookBookRecipes.data.recipesIds.includes(recipeId);
+        } else if (getCookBookRecipes.isError) {
+            setOpen(true);
+            setMessage('An error has occured.');
+        }
+    }, [getCookBookRecipes, recipeId]);
 
     const add = useCallback(() => {
         addRecipe.mutate({ recipeId: recipeId }, {
             onSuccess: () => {
-                const { recipesIds } = queryClient.getQueryData('getCookBookRecipes') as { recipesIds: string[] | undefined };
-                queryClient.setQueryData('getCookBookRecipes', { recipesIds: [...(recipesIds ?? []), recipeId] });
+                const queryData: { recipesIds: Array<string> } | undefined = queryClient.getQueryData('getCookBookRecipes');
+                queryClient.setQueryData('getCookBookRecipes', { recipesIds: [...queryData ? queryData.recipesIds : [], recipeId] });
+            },
+            onError: () => {
+                setOpen(true);
+                setMessage('An error has occured.');
             }
-        }
-        );
-    }, [queryClient, recipeId, addRecipe]);
+        });
+    }, [addRecipe, queryClient, recipeId]);
 
     const remove = useCallback(() => {
         removeRecipe.mutate({ recipeId: recipeId }, {
             onSuccess: () => {
-                const { recipesIds } = queryClient.getQueryData('getCookBookRecipes') as { recipesIds: string[] | undefined };
-                queryClient.setQueryData('getCookBookRecipes', { recipesIds: recipesIds?.filter(e => e !== recipeId) });
+                const queryData: { recipesIds: Array<string> } | undefined = queryClient.getQueryData('getCookBookRecipes');
+                queryClient.setQueryData('getCookBookRecipes', { recipesIds: queryData ? queryData.recipesIds.filter(id => id !== recipeId) : [] });
+            },
+            onError: () => {
+                setOpen(true);
+                setMessage('An error has occured.');
             }
-        }
-        );
-    }, [queryClient, recipeId, removeRecipe]);
+        });
+    }, [removeRecipe, queryClient, recipeId]);
 
     return (
-        <Button sx={{ width: "fit-content" }} onClick={() => isRecipeInCookbook ? remove() : add()} variant="outlined">{isRecipeInCookbook ? 'Unsave recipe' : 'Save recipe'}</Button>
+        <>
+            <Button
+                sx={{ width: "fit-content" }}
+                onClick={() => isRecipeInCookbook ? remove() : add()}
+                variant="outlined"
+            >
+                {isRecipeInCookbook ? 'Unsave recipe' : 'Save recipe'}
+            </Button>
+            <Error open={open} message={message} setOpen={setOpen} setMessage={setMessage} />
+        </>
     );
 }
