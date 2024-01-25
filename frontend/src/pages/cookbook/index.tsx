3ffc25acd5de959474888b5e105b08d013f98dc6
Bruno DE OLIVEIRA <bdoliveira>
2024-01-25 00:32:41 +0000

    Add cookbook

--- frontend/src/pages/cookbook/index.tsx
+++ frontend/src/pages/cookbook/index.tsx
@@ -1,19 +1,46 @@
 import Header from "../../components/Header";
+import RecipeCard from "../../components/RecipeCard";
 import useIsLoggedIn from "../../api/account/useIsLoggedIn";
 import { useNavigate } from "react-router-dom";
+import { CSSProperties } from "react";
+
+type RecipeData = {
+    id: string;
+    name: string;
+    tags: string[];
+    totalPreparationTimeInMinutes: number;
+    avgRating: number;
+    ownerId: string;
+    servings: number;
+}
+
+const GridStyle: CSSProperties = {
+    display: 'grid',
+    gap: '2em',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(21.5em, 1fr))',
+    margin: 'auto',
+    maxWidth: '75%',
+    paddingTop: '2em',
+    placeItems: 'center'
+}
 
 function Cookbook() {
+    const data: Array<RecipeData> = [];
     const auth = useIsLoggedIn();
     const navigate = useNavigate();
-  
+
     if (!auth.data) {
-      navigate('/sign-in')
+        navigate('/sign-in')
     }
-  
+
     return (
         <div>
             <Header />
-            C O O K B O O K
+            <div className='centered' style={GridStyle}>
+                {
+                    data.map((e, index) => <RecipeCard key={index} id={e.id} image={'/assets/veloute-de-giraumon.jpg'} name={e.name} rating={e.avgRating} />)
+                }
+            </div>
         </div>
     );
 }
