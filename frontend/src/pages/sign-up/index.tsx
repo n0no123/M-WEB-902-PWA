52427dd15bac5b170f88a97a419acd75d8610660
Bruno DE OLIVEIRA <bdoliveira>
2024-01-25 18:39:06 +0000

    Add error handling to sign-in/up

--- frontend/src/pages/sign-up/index.tsx
+++ frontend/src/pages/sign-up/index.tsx
@@ -1,92 +1,104 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import useCreateUserAccount from '../../api/account/useCreateUserAccount';
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-
+import CssBaseline from '@mui/material/CssBaseline';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Avatar, Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
+import useCreateUserAccount from '../../api/account/useCreateUserAccount';
+import Error from '../../components/Error';
+import { validateEmail } from '../../utils/EmailUtils';
 
 export default function SignUp() {
+    const [open, setOpen] = useState<boolean>(false);
+    const [message, setMessage] = useState<string>('');
 
-    const hook = useCreateUserAccount();
+    const signUp = useCreateUserAccount();
     const navigate = useNavigate();
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const submit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         const email = data.get('email');
         const password = data.get('password');
+
         if (email && password) {
-            hook.mutate({ email: email.toString(), password: password.toString() });
+            if (validateEmail(email.toString())) {
+                signUp.mutate({ email: email.toString(), password: password.toString() }, {
+                    onSuccess: () => {
+                        navigate('/');
+                    },
+                    onError: () => {
+                        setOpen(true);
+                        setMessage('An error has occured.');
+                    }
+                });
+            } else {
+                setOpen(true);
+                setMessage('Please enter valid email address.');
+            }
+        } else {
+            setOpen(true);
+            setMessage('Please enter valid email address / password.');
         }
     };
 
-    if (hook.isSuccess) {
-        navigate('/sign-in');
-    }
-
     return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign Up
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
+        <>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
                         Sign Up
-                    </Button>
-                    <Grid container>
-                        <Grid item>
-                            <Link href="/sign-in" variant="body2">
-                                {"Already have an account? Sign In"}
-                            </Link>
+                    </Typography>
+                    <Box component="form" onSubmit={submit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container>
+                            <Grid item>
+                                <Link href="/sign-in" variant="body2">
+                                    {"Already have an account? Sign In"}
+                                </Link>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </Box>
                 </Box>
-            </Box>
-        </Container>
+            </Container>
+            <Error open={open} message={message} setOpen={setOpen} setMessage={setMessage} />
+        </>
     );
 }
